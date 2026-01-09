f5c7f85672aee64b4bc6293bf0ef299cfe88cd17
Aravind Vangari <aravind.vangari2@gmail.com>
2026-01-09 20:03:10 +0530

    YCC-01: Added SP_GetEmployees file

---
+++ employeeSerives.Infrastructure/Data/SP_GetEmployees.sql
@@ -0,0 +1,11 @@
+﻿-- =============================================
+-- Author:		Aravind Vangari
+-- Create date: 01-01-2026
+-- Description:	To Get all the Employees Data
+-- =============================================
+CREATE OR ALTER PROCEDURE [dbo].[SP_GetEmployees]
+AS
+BEGIN
+   select * from [dbo].[Employees]
+END
+GO
